e5756b38ed29370a7a0cabcc6c87a5c88d54ec84
@mkusha <delovaakolbasa903@gmail.com>
2026-03-26 09:45:51 -0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3102,7 +3102,7 @@ function Library:CreateWindow(...)
     if type(Config.MenuFadeTime) ~= 'number' then Config.MenuFadeTime = 0.2 end
 
     if typeof(Config.Position) ~= 'UDim2' then Config.Position = UDim2.fromOffset(175, 50) end
-    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(550, 600) end
+    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(620, 600) end
 
     if Config.Center then
         Config.AnchorPoint = Vector2.new(0.5, 0.5)
